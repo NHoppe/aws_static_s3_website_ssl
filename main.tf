9bd7b79f96f16001497229880659d696a48c6570
Natan Hoppe <nhoppe@gmail.com>
2021-09-18 11:46:47 -0400

    Add Cloudfront distribution and its origin, and update S3 bucket policy to restrict access

--- main.tf
+++ main.tf
@@ -80,3 +80,82 @@ resource "aws_acm_certificate_validation" "website" {
   certificate_arn         = aws_acm_certificate.website.arn
   validation_record_fqdns = [for record in aws_route53_record.website_certificate_validation : record.fqdn]
 }
+
+# Setup Cloudfront
+resource "aws_cloudfront_origin_access_identity" "s3_bucket_website" {
+  comment = "Static Corporate Website"
+}
+
+resource "aws_cloudfront_distribution" "website" {
+  origin {
+    domain_name = aws_s3_bucket.website.id
+    origin_id   = "${var.website_domain}-origin"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.s3_bucket_website.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  aliases             = [var.website_domain]
+  price_class         = "PriceClass_100" # Cheapest price class with fewest edge locations
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${var.website_domain}-origin"
+    compress         = true
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 43200 # 12 hours
+    default_ttl            = 43200 # 12 hours
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.website.arn
+    ssl_support_method  = "sni-only" # No dedicated IP, cheaper
+
+    # Balance between security and compatibility
+    # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/secure-connections-supported-viewer-protocols-ciphers.html
+    minimum_protocol_version = "TLSv1.2_2018"
+  }
+}
+
+# Update S3 bucket policy and restrict access with Cloudfront Origin Access Identity
+data "aws_iam_policy_document" "cloudfront_origin_access_identity" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.website.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.s3_bucket_website.iam_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+  policy = data.aws_iam_policy_document.cloudfront_origin_access_identity.json
+}
+
+# Sync repository website files with S3
+resource "null_resource" "sync_with_s3" {
+  provisioner "local-exec" {
+    command = "aws s3 sync ${path.module}/website s3://${aws_s3_bucket.website.id}"
+  }
+}
